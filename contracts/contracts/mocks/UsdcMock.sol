89379745cc5fe5eb5f55fac95d226918184b6695
Matias Zapata <117381223+matzapatasphereone@users.noreply.github.com>
2024-06-18 22:11:25 +0300

    Usdc mock

---
+++ contracts/contracts/mocks/UsdcMock.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract UsdcMock is ERC20, Ownable {
+    constructor(address initialOwner)
+        ERC20("Usdc", "USDC")
+        Ownable(initialOwner)
+    {
+        
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 6;
+    }
+}

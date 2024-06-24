5323dd0738f1361292166207cac006d0b8803abd
Matias Zapata <117381223+matzapatasphereone@users.noreply.github.com>
2024-06-24 22:10:55 +0300

    schema and utils

--- shared/cryptorsa/cryptorsa.go
+++ shared/cryptorsa/cryptorsa.go
@@ -1,6 +1,7 @@
 package cryptorsa
 
 import (
+	"crypto"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha512"
@@ -52,4 +53,21 @@ func LoadPublicKeyFromFile(path string) (*rsa.PublicKey, error) {
 	}
 
 	return LoadPublicKeyFromString(string(data))
+}
+
+func LoadPrivateKeyFromString(privateKeyStr string) (*rsa.PrivateKey, error) {
+	block, _ := pem.Decode([]byte(privateKeyStr))
+	if block == nil || block.Type != "RSA PRIVATE KEY" {
+		return nil, fmt.Errorf("failed to decode PEM block containing private key")
+	}
+
+	return x509.ParsePKCS1PrivateKey(block.Bytes)
+}
+
+func DecryptBytes(privateKey *rsa.PrivateKey, data []byte) ([]byte, error) {
+	// The first argument is an optional random data generator (the rand.Reader we used before)
+	// we can set this value as nil
+	// The OEAPOptions in the end signify that we encrypted the data using OEAP, and that we used
+	// SHA512 to hash the input.
+	return privateKey.Decrypt(nil, data, &rsa.OAEPOptions{Hash: crypto.SHA512})
 }
